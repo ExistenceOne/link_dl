ae5f8a8366e99be26ddd6c0506f2ef974412572e
Youn-Hee Han <yh21.han@gmail.com>
2026-09-20 18:30:52 +0900

    ...

--- _01_code/_05_backpropagation_and_autograd/a_autograd_1.py
+++ _01_code/_05_backpropagation_and_autograd/a_autograd_1.py
@@ -5,7 +5,7 @@ import torch
 w = torch.ones(3, requires_grad=True)
 print(w)  # >>> tensor([1., 1., 1.], requires_grad=True)
 # x is created by the user -> grad_fn is None
-print(w.grad_fn)  # >>> None, None
+print(w.grad, w.grad_fn)  # >>> None, None
 
 print("#" * 50, 1)
 
@@ -22,26 +22,26 @@ print("#" * 50, 2)
 # Do more operations on x
 y = x * 3
 print(y)  # >>> tensor([9., 9., 9.], grad_fn=<MulBackward0>)
-print(y.grad_fn)  # >>> None, <AddBackward0 object at 0x11b082ef0>
+print(y.grad, y.grad_fn)  # >>> None, <AddBackward0 object at 0x11b082ef0>
 
 print("#" * 50, 3)
 
 # Make the output scalar
-z = y.mean()
-print(z)  # >>> tensor(9., grad_fn=<MeanBackward0>)
-print(z.shape)  # >>> torch.Size([])
-print(z.grad_fn)  # >>> None, <MeanBackward0 object at 0x11b082ef0>
+L = y.mean()
+print(L)  # >>> tensor(9., grad_fn=<MeanBackward0>)
+print(L.shape)  # >>> torch.Size([])
+print(L.grad, L.grad_fn)  # >>> None, <MeanBackward0 object at 0x11b082ef0>
 
 
 print("#" * 50, 4)
 
 # x.retain_grad()
 # y.retain_grad()
-# z.retain_grad()
+# L.retain_grad()
 
-z.backward()
+L.backward()
 
-print("z.grad ->", z.grad)
+print("L.grad ->", L.grad)
 print("y.grad ->", y.grad)
 print("x.grad ->", x.grad)
 print("w.grad ->", w.grad)
